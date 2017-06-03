02f056d98fae3a7a2f1da1f899224a7eecc7b0ed
Pavlo <pdyban@gmail.com>
2017-06-03 19:49:48 +0200

    Refactored FT code

--- dicombrowser/test.py
+++ dicombrowser/test.py
@@ -13,38 +13,47 @@ class TestBrowser(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def get_next_test_directory(self):
+        curdir = os.path.dirname(os.path.abspath(__file__))
+        expected_results = {os.path.join(curdir, 'testdirectory/slice'):
+                                {'Patient\'s Name': 'Anonymized', 'Number Of Files': 1,
+                                 'First Filename': 'Anonymized20170603.dcm'},
+                            os.path.join(curdir, 'testdirectory/series'):
+                                {'Patient\'s Name': 'Anonymized', 'Number Of Files': 11,
+                                 'First Filename': 'image-000000.dcm'}
+                            }
+        for subdir, expected_result in expected_results.items():
+            yield subdir, expected_result
+
     def test_can_init_browser(self):
-        directory = "testdirectory"
-        tree = db.browse(directory)
-        self.assertIsNotNone(tree)
-        self.assertIsInstance(tree, dict)
+        for directory, expected_result in self.get_next_test_directory():
+            tree = db.browse(directory)
+            self.assertIsNotNone(tree)
+            self.assertIsInstance(tree, dict)
 
     def test_can_find_all_dicom_files_in_directory(self):
-        dicoms_count = {"testdirectory/slice": 1, "testdirectory/series": 11}
-        for directory in dicoms_count:
-            testdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), directory)
-            tree = db.browse(testdir)
+        for directory, expected_result in self.get_next_test_directory():
+            tree = db.browse(directory)
             self.assertIsNotNone(tree)
-            self.assertEqual(len(tree), dicoms_count[directory])
+            self.assertEqual(len(tree), expected_result['Number Of Files'])
 
     def test_first_file_patientsname(self):
-        fnames = {"testdirectory/slice": ('Anonymized20170603.dcm', 'Anonymized'),
-                  "testdirectory/series": ('image-000000.dcm', 'Anonymized')}
-        for directory in fnames:
-            testdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), directory)
-            tree = db.browse(testdir)
+        for directory, expected_result in self.get_next_test_directory():
+            tree = db.browse(directory)
 
-            first_filename = os.path.join(testdir, fnames[directory][0])
-
-            self.assertEqual(tree[first_filename]['Patient\'s Name'], fnames[directory][1])
+            first_filename = os.path.join(directory, expected_result['First Filename'])
+            self.assertEqual(tree[first_filename]['Patient\'s Name'], expected_result['Patient\'s Name'])
 
     def test_keeps_alphabetic_sorting(self):
-        subdirs = ("testdirectory/slice", "testdirectory/series")
-        for directory in subdirs:
-            testdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), directory)
-            tree = db.browse(testdir)
+
+        for directory, expeced_result in self.get_next_test_directory():
+            tree = db.browse(directory)
 
             self.assertListEqual(sorted(tree.keys()), list(tree.keys()))
+
+    def test_reads_tag_values_correctly(self):
+        pass
+
 
 
 if __name__ == "__main__":

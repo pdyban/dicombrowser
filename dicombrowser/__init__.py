f1ac996956c36ed12b08fa3ec07ecbc2d146048d
Pavlo <pdyban@gmail.com>
2017-06-03 18:47:42 +0200

    Added FT that can init instance

---
+++ dicombrowser/__init__.py
@@ -0,0 +1,9 @@
+
+def browse(directory):
+    """
+    Browses a directory and returns list of DICOM files and the values of their tags as a dictionary.
+
+    :param directory: directory pth where to search for DICOM files.
+    :return: dictionary with DICOM tag values for each DICOM file in directory
+    """
+    return {}
--- dicombrowser/test.py
+++ dicombrowser/test.py
@@ -1,5 +1,22 @@
 import unittest
+import dicombrowser as db
 
+
+class TestBrowser(unittest.TestCase):
+    # def __init__(self):
+    #    pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_can_init_browser(self):
+        directory = "testdirectory"
+        tree = db.browse(directory)
+        self.assertIsNotNone(tree)
+        self.assertIsInstance(tree, dict)
 
 if __name__ == "__main__":
     unittest.main()

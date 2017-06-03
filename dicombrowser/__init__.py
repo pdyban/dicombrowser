0c5d60cf3bcaadac4967733958eec1c70065ea01
Pavlo <pdyban@gmail.com>
2017-06-03 19:16:16 +0200

    Added FT to parse all DICOM files in directory

--- dicombrowser/__init__.py
+++ dicombrowser/__init__.py
@@ -1,3 +1,6 @@
+import os
+import dicom
+
 
 def browse(directory):
     """
@@ -6,4 +9,28 @@ def browse(directory):
     :param directory: directory pth where to search for DICOM files.
     :return: dictionary with DICOM tag values for each DICOM file in directory
     """
-    return {}
+    tree = {}
+    for fname in os.listdir(directory):
+        full_fname = os.path.join(directory, fname)
+        if is_dicom_file(full_fname):
+            tree[full_fname] = ''
+    return tree
+
+
+def is_dicom_file(fname):
+    """
+    Returns true if the specified file is a valid DICOM file.
+
+    :param fname: path to file
+    :return: True, if file is a valid DICOM file; False, otherwise.
+    """
+    if not os.path.isfile(fname):
+        return False
+
+    try:
+        dicom.read_file(fname, stop_before_pixels=True)
+
+    except dicom.errors.InvalidDicomError as e:
+        return False
+
+    return True
--- dicombrowser/test.py
+++ dicombrowser/test.py
@@ -1,5 +1,6 @@
 import unittest
 import dicombrowser as db
+import os
 
 
 class TestBrowser(unittest.TestCase):
@@ -17,6 +18,15 @@ class TestBrowser(unittest.TestCase):
         tree = db.browse(directory)
         self.assertIsNotNone(tree)
         self.assertIsInstance(tree, dict)
+
+    def test_can_find_all_dicom_files_in_directory(self):
+        directories = {"testdirectory/slice": 1, "testdirectory/series": 361}
+        for directory in directories:
+            testdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), directory)
+            tree = db.browse(testdir)
+            self.assertIsNotNone(tree)
+            self.assertEqual(len(tree), directories[directory])
+
 
 if __name__ == "__main__":
     unittest.main()

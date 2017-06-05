42ef5b4a38091a0234e02e88a3e8032c63f03fbd
Pavlo <pdyban@gmail.com>
2017-06-05 13:37:32 +0200

    Added missing file to last commit

---
+++ dicomviewer/model.py
@@ -0,0 +1,17 @@
+from dicombrowser import browse
+
+
+class Model(list):
+    """
+    Encapsulates the model that is used in MVVM pattern.
+    """
+    def __init__(self, directory, select_tags):
+        super(Model, self).__init__()
+        self.directory = directory
+        self.select_tags = select_tags
+        self.viewmodels = []
+
+        if select_tags is None or len(select_tags) < 1:
+            raise AttributeError("Please specify at least one DICOM tag.")
+
+        self.items = browse(directory, select_tags=select_tags)
